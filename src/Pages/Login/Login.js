9c26391c37a3848948520a53f7baae35d4072504
Rabbi Ahmed Limon <rabbiahemed>
2023-02-15 16:49:21 +0600

    basic login form

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -3,7 +3,53 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { Link } from "react-router-dom";
 const Login = () => {
-  return <div className="w-50 mx-auto my-5"></div>;
+  return (
+    <div className="w-50 mx-auto my-5">
+      <h2 className="text-center">Login</h2>
+
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Control
+            type="email"
+            name="email"
+            placeholder="Enter email"
+            required
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Control
+            type="password"
+            name="password"
+            placeholder="Password"
+            required
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Login
+        </Button>
+        <p className="small fw-bold mt-2 pt-1 mb-0">
+          Don't have an account?
+          <Link to="/register" className="link-success">
+            Register now
+          </Link>
+        </p>
+      </Form>
+
+      <div>
+        <h6 className="mt-2">or</h6>
+
+        <Button
+          //   onClick={handleGoogleSignIn}
+          type="submit"
+          variant="warning"
+          className="fw-bold p-3"
+        >
+          Sign in with Google
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
